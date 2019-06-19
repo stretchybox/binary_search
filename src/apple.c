b7575b7b8adf5c1a3ff5289402235e3a2c1c0d74
https://github.com/alg2019/binary_search.git <>
2019-06-19 14:28:09 +0900

    8.1,8.2

--- src/apple.c
+++ src/apple.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int n;
 int k;
 int A[100000];
 
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
+bool ok(int x) {
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += (A[i] - 1) / x + 1;
+	}
+	return sum <= k;
+}
 
-
-  return 0;
+int main() {
+	int i, lb, ub;
+	scanf("%d%d", &n, &k);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
+	lb = 0, ub = 1e9;
+	while (ub - lb > 1) {
+		int mid = (lb + ub) / 2;
+		if (ok(mid)) {
+			ub = mid;
+		} else {
+			lb = mid;
+		}
+	}
+	printf("%d\n", ub);
+	return 0;
 }
--- src/array.c
+++ src/array.c
@@ -4,15 +4,21 @@ int n;
 int k;
 int A[100000];
 
-
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-
-  return 0;
+int main() {
+	int i, lb, ub;
+	scanf("%d%d", &n, &k);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
+	lb = -1, ub = n;
+	while (ub - lb > 1) {
+		int mid = (lb + ub) / 2;
+		if (A[mid] >= k) {
+			ub = mid;
+		} else {
+			lb = mid;
+		}
+	}
+	printf("%d\n", ub);
+	return 0;
 }
