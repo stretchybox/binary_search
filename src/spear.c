bae1002ea57595475ad18a4db4afb755d8f4d853
https://github.com/alg2019/binary_search.git <>
2019-06-19 15:34:21 +0900

    8.3,8.4

--- src/spear.c
+++ src/spear.c
@@ -1,17 +1,36 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int n;
 int k;
 int A[100000];
 
+bool ok(int x) {
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += A[i] / x;
+	}
+	return sum >= k;
+}
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+int main() {
+	int i, lb, ub;
+	scanf("%d%d", &n, &k);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
+	lb = 1, ub = 1e9 + 1;
+	while (ub - lb > 1) {
+		int mid = (ub + lb) / 2;
+		if (ok(mid)) {
+			lb = mid;
+		} else {
+			ub = mid;
+		}
+	}
+	if (!ok(lb)) {
+		lb = 0;
+	}
+	printf("%d\n", lb);
+	return 0;
 }
--- src/works.c
+++ src/works.c
@@ -1,17 +1,43 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int n;
 int k;
 int A[100000];
 
+bool ok(int x) {
+	int cnt = 1;
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		if (A[i] > x) {
+			return false;
+		}
+		if (sum + A[i] > x) {
+			sum = A[i];
+			cnt++;
+		} else {
+			sum += A[i];
+		}
+	}
+	return cnt <= k;
+}
 
-int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
-  return 0;
+int main() {
+	int i, lb, ub;
+	scanf("%d%d", &n, &k);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &A[i]);
+	}
+	lb = 0;
+	ub = 1e9;
+	while (ub - lb > 1) {
+		int mid = (ub + lb) / 2;
+		if (ok(mid)) {
+			ub = mid;
+		} else {
+			lb = mid;
+		}
+	}
+	printf("%d\n", ub);
+	return 0;
 }
